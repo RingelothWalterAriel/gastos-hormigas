1f964eef54f89c4ba83c42841f9e78e79dbd764a
walter ringeloth <walter@walter>
2026-09-15 17:47:07 -0300

    actualizacion

--- main.py
+++ main.py
@@ -6,24 +6,39 @@ ventana.geometry("500x400")
 ventana.resizable(False, False)
 ventana.configure(bg="#009cea")
 
+def saludar():
+    nombre = entrada_texto.get().strip()
+    saludo_salida.config(text=f"Hola, {nombre}")
+
 tk.Label(
     ventana,
     text = "Hola, ingrese su nombre",
     font = ("Arial", 14),
   
-).pack(pady=(20, 0), padx= 10,)
+).pack(pady= 20, padx= 10,)
 
-entrada_texto=tk.Entry(ventana)
+entrada_texto=tk.Entry(
+    ventana,
+    font=("Arial", 14) 
+    )
 entrada_texto.pack()                      
 
 
 boton = tk.Button(
     ventana,
-    text= "Aceptar",
+    text= "Saludar",
     font=("Arial",14),
-    cursor="hand2"
+    cursor="hand2",
+    command=saludar
                 )
-boton.pack ()
+boton.pack (pady = 20)
+ 
   
+saludo_salida = tk.Label(
+ventana,
+    font = ("Arial", 14),
+ )
+
+saludo_salida.pack()
 
 ventana.mainloop()
